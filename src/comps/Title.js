8694e9847466bb1abdbb8ad90ac396803250a353
Ahmar Ali Khan <mail2ahmaralikhan52@gmail.com>
2021-01-12 02:12:21 +0530

    Link Changed

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
-      <p>Developed by <a href="https://ahmar46.github.io/ahmar_portfolio">(mr.electrifying)</a></p>
+      <p>Developed by <a href="https://ahmar46.github.io/portfolio">(mr.electrifying)</a></p>
       <h2>Post your Pictures</h2>
       <p>A prototype of a future billon dollar company.</p>
     </div>
